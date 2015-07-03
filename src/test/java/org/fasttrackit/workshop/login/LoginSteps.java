6a4d85066447d224f80a0d066134bb4a382bb2b8
IoanaIoaniciu <ioana.ioaniciu@gmail>
2015-07-03 16:34:11 +0300

    Final implementaion of the first scenario, Logged In Success

--- src/test/java/org/fasttrackit/workshop/login/LoginSteps.java
+++ src/test/java/org/fasttrackit/workshop/login/LoginSteps.java
@@ -1,5 +1,6 @@
 package org.fasttrackit.workshop.login;
 
+import com.sdl.selenium.web.utils.Utils;
 import cucumber.api.PendingException;
 import cucumber.api.java.en.And;
 import cucumber.api.java.en.Given;
@@ -7,49 +8,57 @@ import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 import org.fasttrackit.util.TestBaseNative;
 import org.fasttrackit.workshop.pagefactory.login.LoginPage;
+import org.junit.Assert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
 
 public class LoginSteps extends TestBaseNative {
     private static final Logger LOGGER = LoggerFactory.getLogger(LoginSteps.class);
 
     LoginPage loginPage;
 
-    @Given("^I open login page$")
-    public void I_open_login_page() {
-        driver.get("file:///D:/Testy/src/test/functional/login.html");
-
-        //loginPage = PageFactory.initElements(driver, LoginPage.class);
+    @Given("^I access the login page$")
+    public void I_access_the_login_page() throws Throwable {
+        driver.get("https://dl.dropboxusercontent.com/u/16174618/FastTrackIT/app-demo/login.html");
     }
 
-    @When("^I enter email \"([^\"]*)\"$")
-    public void I_enter_email(String email) {
-        WebElement element = driver.findElement(By.id("email"));
-        element.sendKeys(email);
-    }
 
     @And("^I insert valid credentials$")
     public void I_insert_valid_credentials() throws Throwable {
-        System.out.println("I insert valid credentials");
+       WebElement email = driver.findElement(By.id("email"));
+        email.sendKeys("eu@fast.com");
+
+        WebElement password = driver.findElement(By.id("password"));
+        password.sendKeys("eu.pass");
+
     }
 
-    @Given("^I access the login page$")
-    public void I_access_the_login_page() throws Throwable {
-        System.out.println("aici ar trebuie sa intru pe pagina mea");
-    }
+
 
     @When("^I click login button$")
     public void I_click_login_button() throws Throwable {
-        // Express the Regexp above with the code you wish you had
-        throw new PendingException();
+        WebElement loginButton = driver.findElement(By.id("loginButton"));
+        loginButton.click();
+        System.out.println("Login succesfull");
+
     }
 
     @Then("^I check if user was logged in$")
     public void I_check_if_user_was_logged_in() throws Throwable {
-        // Express the Regexp above with the code you wish you had
-        throw new PendingException();
+        //WebElement logout = driver.findElement(By.linkText("Logout"));
+        //boolean successLoggedIn = logout.isDisplayed();
+        //assertThat("Could not find logout button", successLoggedIn, is(true));
+        boolean successLoggedIn = false;
+        try{
+            WebElement logout = driver.findElement(By.linkText("Logout"));
+            successLoggedIn = logout.isDisplayed();
+        } catch (Exception e){}
+        assertThat("Could not find logout button", successLoggedIn, is(true));
     }
 
     @And("^I insert invalid credentials$")
--- src/test/resources/feature/workshop/login.feature
+++ src/test/resources/feature/workshop/login.feature
@@ -1,8 +1,8 @@
 Feature: Login
 
-  Scenario: Open any web page
-    Given I open this url "http://google.com"
-    Then I send 10 into search field
+  //Scenario: Open any web page
+   // Given I open this url "https://dl.dropboxusercontent.com/u/16174618/FastTrackIT/app-demo/login.html"
+    //Then I send 10 into search field
 
   Scenario: Login Succesfully
     Given I access the login page
